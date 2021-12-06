e2eb8bbbd2bce9acf1a94cd698f4fb4ffe169614
youssefselmi <youssef.selmi@esprit.tn>
2021-12-06 23:59:29 +0100

    Modification

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,19 @@ import { NotfoundpageComponent } from './notfoundpage/notfoundpage.component';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FournisseurComponent } from './fournisseur/fournisseur.component';
+import { DetailFournisseurComponent } from './detail-fournisseur/detail-fournisseur.component';
+import { ListfournisseurComponent } from './listfournisseur/listfournisseur.component';
+import { ListerayonComponent } from './listerayon/listerayon.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'facture', component: FactureComponent },
+  { path: 'fournisseur', component: FournisseurComponent },
+  { path: 'adminfournisseur', component: ListfournisseurComponent },
+  { path: 'adminrayon', component: ListerayonComponent },
+  {path: 'home/fournisseur/:id/:libelle/:date_fin_contrat/:likeFournisseur/:email_fournisseur', component: DetailFournisseurComponent},
   { path: '**', component: NotfoundpageComponent },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,30 @@ import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { NotfoundpageComponent } from './notfoundpage/notfoundpage.component';
 import { FactureComponent } from './facture/facture.component';
+import { FournisseurComponent } from './fournisseur/fournisseur.component';
+import { DetailFournisseurComponent } from './detail-fournisseur/detail-fournisseur.component';
+import { TemplateFormFournisseurComponent } from './template-form-fournisseur/template-form-fournisseur.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ListfournisseurComponent } from './listfournisseur/listfournisseur.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { Ng2OrderModule } from 'ng2-order-pipe';
+import { NgxQRCodeModule } from 'ngx-qrcode2';
+import { QRCodeModule } from 'angularx-qrcode';
+import { ListerayonComponent } from './listerayon/listerayon.component';
+import { TemplateFormRayonComponent } from './template-form-rayon/template-form-rayon.component';
+import { NgImageSliderModule } from 'ng-image-slider';
+import { SliderfournisseurComponent } from './sliderfournisseur/sliderfournisseur.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
+
+
+
+
+
+
 
 @NgModule({
   declarations: [
@@ -14,12 +38,32 @@ import { FactureComponent } from './facture/facture.component';
     HomeComponent,
     HeaderComponent,
     NotfoundpageComponent,
-    FactureComponent
+    FactureComponent,
+    FournisseurComponent,
+    DetailFournisseurComponent,
+    TemplateFormFournisseurComponent,
+    ListfournisseurComponent,
+    ListerayonComponent,
+    TemplateFormRayonComponent,
+    SliderfournisseurComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    NgxPaginationModule,
+    Ng2SearchPipeModule,
+    Ng2OrderModule,
+    NgxQRCodeModule,
+    ReactiveFormsModule,
+    QRCodeModule,
+    NgImageSliderModule,
+    NgbModule,
+    
+ ],
+
+
   providers: [],
   bootstrap: [AppComponent]
 })
